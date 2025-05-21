17bb7f684cdb612ef5e0f6398356338ed66e67f7
Zach Price <zach.price105@gmail.com>
2025-05-21 13:08:50 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AI Power Grid Documentation
+
+This is the documentation for the AI Power Grid project.
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>AI Power Grid</span>,
+  project: {
+    link: 'https://github.com/AIPowerGrid/aipg-documentation',
+  },
+  chat: {
+    link: 'https://discord.gg/W9D8j6HCtC',
+  },
+  docsRepositoryBase: 'https://github.com/AIPowerGrid/aipg-documentation',
+  footer: {
+    text: 'AI Power Grid Documentation',
+  },
+}
+
+export default config
